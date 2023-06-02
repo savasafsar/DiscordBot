3267f8c020420cec18c0069fb207cd69b4924071
savasafsar <savasafsar3443@gmail.com>
2023-06-02 20:50:57 +0300

    errormessageadd

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from discord.ext import commands
 
 logger = settings.logging.getLogger("bot")
 
+class Slapper(commands.Converter):
+    use_nicknames : bool
+    def __init__(self, use_nicknames=False) -> None:
+        self.use_nicknames = use_nicknames
+
+    async def convert(self, ctx, argument):
+        someone = random.choice(ctx.guild.members)
+        nickname = ctx.author
+        if self.use_nicknames:
+            nickname = ctx.author.nick
+        return f"{nickname} slaps {someone} with {argument}"
+
 def run():
     intents = discord.Intents.default()
     intents.message_content = True
@@ -39,13 +51,26 @@ def run():
     async def choices(ctx, *options):
         await ctx.send(random.choice(options))
 
+
     @bot.command()
     async def add(ctx, one: int , two: int):
         await ctx.send(one+two)
-         
+
+    @add.error
+    async def add_error(ctx,error):
+        if isinstance(error,commands.MissingRequiredArgument):
+         await  ctx.send("Hatali Kullanim 2 Sayiyi Giriniz")   
 
     @bot.command()
-    async def say3(ctx, what="WHAT?",why ="WHY?" ):
+    async def joined(ctx, who: discord.Member):
+        await ctx.send(who.joined_at)
+
+    @bot.command()
+    async def slap(ctx, reason: Slapper(use_nicknames=False)):
+     await ctx.send(reason)
+
+    @bot.command()
+    async def say3(ctx, what="WHAT?", why="WHY?"):
         await ctx.send(what+why)
     
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
